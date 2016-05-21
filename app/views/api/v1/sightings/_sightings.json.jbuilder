f801805d599a3e58bf23524bdbf8080eecfedd63
Bejan Maljai <bmaljai@gmail.com>
2016-05-21 16:01:23 -0700

    fixed partial and added seed file

--- app/views/api/v1/sightings/_sightings.json.jbuilder
+++ app/views/api/v1/sightings/_sightings.json.jbuilder
@@ -1,5 +1,5 @@
 json.id sighting.id
 json.track_number sighting.track_number
-json.total_sighting sighting.total_sighting
+json.total_homeless sighting.total_homeless
 json.living_type sighting.living_type
 json.city sighting.city
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+50.times do
+  Sighting.create(
+    track_number: Faker::Number.number(4),
+    total_homeless: Faker::Number.number(1),
+    living_type: ["car","rv","street"][rand(0..2)],
+    city: ["San Mateo", "Redwood City", "Belmont"][rand(0..2)],
+    )
+end
